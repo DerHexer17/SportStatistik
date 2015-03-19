be1b20eec9895cb5f361d437959c6f6dbd4993ed
DerHexer17 <hendrik.thues@gmail.com>
2015-03-19 17:39:37 +0100

    Versuch des Navigations Drawers

---
+++ app/src/main/java/android/ht/sportstatistik/activities/SpielFragment.java
@@ -0,0 +1,169 @@
+package android.ht.sportstatistik.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.ht.sportstatistik.datahandling.DatabaseHelper;
+import android.ht.sportstatistik.datahandling.Spiel;
+import android.ht.sportstatistik.datahandling.Spieler;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.ht.sportstatistik.R;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SpielFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SpielFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SpielFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+    DatabaseHelper dbh;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SpielFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SpielFragment newInstance(String param1, String param2) {
+        SpielFragment fragment = new SpielFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SpielFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        dbh = DatabaseHelper.getInstance(getActivity().getApplicationContext());
+        ListView lv = (ListView) getActivity().findViewById(R.id.listSpieler);
+        lv.setAdapter(new SpieleAdapter(getActivity().getApplicationContext(), (List<Spieler>) dbh.getAllPlayers()));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_spiel, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(int position) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(position);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(int position);
+    }
+
+    private class SpieleAdapter extends BaseAdapter {
+
+        Context context;
+        List<Spieler> spiele;
+        public SpieleAdapter(Context context, List<Spieler> spiele){
+            this.context = context;
+            this.spiele = spiele;
+        }
+        @Override
+        public int getCount() {
+            return spiele.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return spiele.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater mInflater = (LayoutInflater)
+                        context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+                convertView = mInflater.inflate(R.layout.fragment_spiel, null);
+            }
+
+
+            TextView txtTitle = (TextView) convertView.findViewById(R.id.textSpiele);
+
+
+            txtTitle.setText(spiele.get(position).getNachname());
+
+            return convertView;
+        }
+    }
+
+}
